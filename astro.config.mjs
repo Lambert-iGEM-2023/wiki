ac7655e97b6299d0304bc80f58fc4663bb691636
Ryan Du <ryandu9221@gmail.com>
2023-06-27 16:26:14 -0400

    updated config file to deploy to gitlab pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()]
+  integrations: [react(), tailwind(), mdx()],
+  sitemap: true,
+  site: 'https://2023.igem.wiki/lambert-ga/',
+  outDir: 'public',
+  publicDir: 'static',
 });
